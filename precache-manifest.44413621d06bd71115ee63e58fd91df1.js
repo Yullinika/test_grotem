d461856f070e76670bd185317a388d17d81f4bb3
Admin <yunova2552@gmail.com>
2018-11-20 13:23:32 +0300

    Updates

---
+++ precache-manifest.44413621d06bd71115ee63e58fd91df1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "128437bcc2e9f48d2f7d",
+    "url": "/test_grotem/static/js/runtime~main.128437bc.js"
+  },
+  {
+    "revision": "dca4626948165335da88",
+    "url": "/test_grotem/static/js/main.dca46269.chunk.js"
+  },
+  {
+    "revision": "20bc491bae5e667480bb",
+    "url": "/test_grotem/static/js/1.20bc491b.chunk.js"
+  },
+  {
+    "revision": "dca4626948165335da88",
+    "url": "/test_grotem/static/css/main.4fd2825d.chunk.css"
+  },
+  {
+    "revision": "a6672f0a4bad40776842439f7c3b9e60",
+    "url": "/test_grotem/index.html"
+  }
+];
